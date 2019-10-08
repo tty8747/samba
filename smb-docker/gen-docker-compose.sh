e600a5fd69526313b0281a1dfc239af2a1a431b9
Sergey Chulanov <tty8747@gmail.com>
2019-10-09 00:27:15 +0300

    Added script generate to docker-compose.yml. Mapping isn't working now

--- smb-docker/gen-docker-compose.sh
+++ smb-docker/gen-docker-compose.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-user = user
-dc = docker-compose.yml
-count = 10
+user=user # user names
+dc=docker-compose.yml # output file
+count=5 # user quantity
+p_len=12 # pass length
+smb_root=./raid
+public=public
 
 touch ./$dc
 
@@ -16,10 +19,10 @@ echo "      TZ: 'EST5EDT'" >> ./$dc
 echo "    networks:" >> ./$dc
 echo "      - default" >> ./$dc
 echo "    ports:" >> ./$dc
-echo "      - "137:137/udp"" >> ./$dc
-echo "      - "138:138/udp"" >> ./$dc
-echo "      - "139:139/tcp"" >> ./$dc
-echo "      - "445:445/tcp"" >> ./$dc
+echo "      - \"137:137/udp\"" >> ./$dc
+echo "      - \"138:138/udp\"" >> ./$dc
+echo "      - \"139:139/tcp\"" >> ./$dc
+echo "      - \"445:445/tcp\"" >> ./$dc
 echo "    read_only: true" >> ./$dc
 echo "    tmpfs:" >> ./$dc
 echo "      - /tmp" >> ./$dc
@@ -27,16 +30,20 @@ echo "    restart: unless-stopped" >> ./$dc
 echo "    stdin_open: true" >> ./$dc
 echo "    tty: true" >> ./$dc
 echo "    volumes:" >> ./$dc
-echo "      - /mnt:/mnt:z" >> ./$dc
-echo "      - /mnt2:/mnt2:z" >> ./$dc
-for (( i = 1; i <= 10; i++ ))
+mkdir -p $smb_root/$public
+echo "      - $smb_root/$public:/mnt/$public" >> ./$dc
+
+for (( i = 1; i <= $count; i++ ))
   do
-#    echo -n "command: -s \"Mount;/mnt\" -s "$user$i Volume;/mnt2;" >> ./$dc
-#    echo -n 'y es;' >> ./$dc
-#    echo "no;no;bob -u "$user;$(pwgen 12 1)"" >> ./$dc
+    mkdir -p $smb_root/$user$i
+    echo "      - $smb_root/users/$user$i:/mnt/$user$i" >> ./$dc
   done
+
+for (( i = 1; i <= $count; i++ ))
+  do
+    echo "    command: '-s \"Mount;/mnt\" -s \"$user$i Volume;/mnt/$user$i;no;no;no;;$user$i\" -u \"$user$i;$(pwgen $p_len 1)\"'" >> ./$dc
+  done
+
 echo  >> ./$dc
 echo "networks:" >> ./$dc
 echo "  default:" >> ./$dc
-
-# user=user; for i in {1..5}; do echo command: -s "Mount;/mnt" -s "$user$i Volume;/mnt2;yes;no;no;bob" -u "$user;$(pwgen 12 1)"  ; done
